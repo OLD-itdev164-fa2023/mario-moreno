0164dc7589d640ec08d61d6187480e988f9094c8
morenm14 <morenm14@gmatc.matc.edu>
2022-09-12 13:07:08 -0500

    Updated blog-post.js to display the body

--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -3,10 +3,13 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
 function BlogPost({ data }) {
-  const { title } = data.contentfulBlogPost
+  const { title, body } = data.contentfulBlogPost
   return (
     <Layout>
       <h1>{title}</h1>
+      <div
+        dangerouslySetInnerHTML={{ __html: body.childMarkdownRemark.html }}
+      ></div>
     </Layout>
   )
 }
@@ -18,6 +21,11 @@ export const pageQuery = graphql`
     contentfulBlogPost(slug: { eq: $slug }) {
       title
       slug
+      body {
+        childMarkdownRemark {
+          html
+        }
+      }
     }
   }
 `
