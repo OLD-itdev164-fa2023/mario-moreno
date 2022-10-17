dcec2aa536cc7cd7ace7b01c1c823fd7df89af1a
morenm14 <morenm14@gmatc.matc.edu>
2022-10-17 10:35:14 -0500

    completed activity-6

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,26 +1,28 @@
 import * as React from "react"
 import { graphql, Link } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import * as styles from "../components/index.module.css"
+import { List, ListItem } from "../components/List"
 
 const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
 
-    <ul className={styles.list}>
+    <List width={[1, 2 / 3, 7 / 8]} p={2}>
       {data.allContentfulBlogPost.edges.map(edge => (
-        <li key={edge.node.id} className={styles.listItem}>
+        <ListItem key={edge.node.id} p={3}>
           <Link to={edge.node.slug}>{edge.node.title}</Link>
           <div>
-            <GatsbyImage image={edge.node.heroImage.gatsbyImageData} />
+            <GatsbyImage
+              image={edge.node.heroImage.gatsbyImageData}
+              alt={edge.node.title}
+            />
           </div>
           <div>{edge.node.body.childMarkdownRemark.excerpt}</div>
-        </li>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   </Layout>
 )
 
@@ -45,7 +47,7 @@ export const query = graphql`
             gatsbyImageData(
               layout: CONSTRAINED
               placeholder: BLURRED
-              width: 300
+              width: 600
             )
           }
         }
