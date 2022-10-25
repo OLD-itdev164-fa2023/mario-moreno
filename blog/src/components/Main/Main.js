683bca74ed48b3fc787895ab15c201ad2d8fc968
morenm14 <morenm14@gmatc.matc.edu>
2022-10-25 10:16:34 -0500

    Created Main, Section, and Footer components

---
+++ blog/src/components/Main/Main.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { BaseContainer } from "../BaseContainer"
+
+import React from "react"
+
+export const Main = props => {
+  return <BaseContainer as="main" {...props} />
+}
---
+++ blog/src/components/Section/Section.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { BaseContainer } from "../BaseContainer"
+
+import React from "react"
+
+export const Section = props => {
+  return <BaseContainer as="section" {...props} />
+}
